9a22462c25a07a71df2181ab8bd2984371423449
czy4066 <czy4066@outlook.com>
2026-09-10 16:28:32 +0800

    初步完成subscriber节点的编写

--- final_mission/src/pubsub_demo/pubsub_demo/subscriber_demo_node.py
+++ final_mission/src/pubsub_demo/pubsub_demo/subscriber_demo_node.py
@@ -1,8 +1,41 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import json
 class SubNode(Node):
     def __init__(self):
         super().__init__("subscriber_node")
         self.points=[[0,0],[2,0],[4,1],[4,3],[2,4],[0,4],[-1,2],[-1,1]]
-        self.connect=[3,4,5,6,7,0,1]
+        self.connect=[3,4,5,6,7,0,1,2]
+        self.sub=self.create_subscription(String,"pubsub_topic",self.callback,10)
+        self.last_state=None
+        self.vx=0.0
+        self.vy=0.0
+        self.get_logger().info("Subscriber节点已启动")
+    def callback(self,msg):
+        try:
+            data=json.loads(msg.data)
+        except json.JSONDecodeError:
+            self.get_logger().warn("收到非法json消息，跳过")
+            return
+        state=data["state"]
+        self.vx=data["vx"]
+        self.vy=data["vy"]
+        current_idx=data["current_idx"]
+        next_idx=data["next_idx"]
+        self.get_logger().info(f"接收成功")
+        if state=="move":
+            # 此处应该是一些控制小车电机转速来实现具体速度方向及大小的实际调整
+            self.get_logger().info("move-ing")
+        elif state=="arrive":
+            #此处应该是控制小车电机停转的代码
+            self.get_logger().info(f"arrive{current_idx}!即将前往{next_idx}")
+        self.last_state=state 
+def main(args=None):
+    rclpy.init(args=args)
+    node=SubNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+if __name__=="__main__":
+    main()
